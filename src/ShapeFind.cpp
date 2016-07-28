848000d711993cec8902c6bfa2baf62b33899ed4
RustyBamboo <dvolya@gmail.com>
2016-07-28 18:20:10 -0400

    ShapeFinder code

--- src/ShapeFind.cpp
+++ src/ShapeFind.cpp
@@ -1,1 +1,45 @@
 #include "ShapeFind.h"
+
+ShapeFind::ShapeFind(Color C) { parseColor = c; }
+ShapeFind::void FindContours() { findContours(binary_frame, contoursfindMat, hierarchyfindMat, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0)); }
+ShapeFind::void FindShapes() {
+    for (int i = 0; i < contoursfindMat.size(); i++) {
+        std::vector<cv::Point> approx;
+        approxPolyDP(Mat(contoursfindMat[i]), approx, arcLength(Mat(contoursfindMat[i]), true) * 0.025, true);
+        shapes.push_back(approx);
+    }
+}
+
+Point findCenter(std::vector<cvPoint> &points) {
+    int x = 0, y = 0;
+    for (int i = 0; i < approx.size(); i++) {
+        x += approx[i].x;
+        y += approx[i].y;
+    }
+    x /= approx.size();
+    y /= approx.size();
+    return Point(x, y);
+}
+
+ShapeFind::std::vector<Result> GetShapes(Mat frame) {
+    Mat binary_frame = frame;
+    FindContours();
+    FindShapes();
+    std::vector<Result> holder;
+    for (int i = 0; i < shapes.size(); i++) {
+        if (shapes[i].approx.size() == 12) {
+            Result hold;
+            hold.center = findCenter(shapes[i].approx.size());
+            hold.Color = parseColor;
+            hold.Shape = CROSS;
+            holder.push_back(hold);
+        } else if (shapes[i].approx.size() == 3) {
+            Result hold;
+            hold.center = findCenter(shapes[i].approx.size());
+            hold.Color = parseColor;
+            hold.Shape = CROSS;
+            holder.push_back(hold);
+        }
+    }
+    return holder;
+}
--- src/ShapeFind.h
+++ src/ShapeFind.h
@@ -1,34 +1,25 @@
+#ifndef SHAPE_FIND_HPP
+#define SHAPE_FIND_HPP
+// Finds shapes in Binary frame
+class ShapeFind {
+  private:
+    Color parseColor;
+    Mat binary_frame;
+    std::vector<std::vector<Point>> contoursfindMat;
+    std::vector<Vec4i> hierarchyfindMat;
+    std::vector<std::vector<cv::Point>> shapes;
+    Point findCenter(std::vector<Point> points);
+    void FindContours();
+    void FindShapes();
 
-//Finds shapes in Binary frame
-class ShapeFind
-{
-	private:
-		Color myColor;
-		Mat binary_frame;
-      	std::vector<std::vector<Point> > contoursfindMat;
-		std::vector<Vec4i> hierarchyfindMat;
-		std::vector < std::vector<cv::Point> > shapes;
-		st
-		void FindContours();
-		void FindShapes();
-	public:
-		typedef struct Result
-		{
-			Point center;
-			enum Color
-			{
-				RED,
-				BLUE,
-				GREEN,
-				BLACK
-			}
-			enum Shape
-			{
-				CROSS,
-				TRIANGLE,
-				CIRLCE
-			}
-		}
-		ShapeFind(Color c);
-		std::vector<Result> GetShapes(Mat frame);		
+  public:
+    typedef struct Result {
+        Point center;
+        enum Color { RED, BLUE, GREEN, BLACK };
+        enum Shape { CROSS, TRIANGLE, CIRLCE };
+    };
+    ShapeFind(Color c);
+    std::vector<Result> GetShapes(Mat frame);
 };
+
+#endif
