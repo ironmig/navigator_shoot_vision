c26496e22c27bd295db978ae161dc676b86b6188
RustyBamboo <dvolya@gmail.com>
2016-08-18 21:42:08 -0400

    Added a service to get X,Y given Shape+Color

--- src/smart.cpp
+++ src/smart.cpp
@@ -16,45 +16,42 @@
 #include "navigator_shoot_vision/GetShape.h"
 #include "navigator_shoot_vision/Symbols.h"
 
-  	   navigator_shoot_vision::Symbols syms;
+navigator_shoot_vision::Symbols syms;
 class ImageSearcher {
   private:
-
     ros::NodeHandle n;
     ros::Subscriber sub;
     ros::ServiceServer service;
 
   public:
-
     ImageSearcher() {
-    	syms = navigator_shoot_vision::Symbols();
+        syms = navigator_shoot_vision::Symbols();
         sub = n.subscribe("found_shapes", 1000, chatterCallback);
         service = n.advertiseService("GetShape", test);
     }
-   static void chatterCallback(const navigator_shoot_vision::Symbols &symbols) { 
-   syms = symbols;
-//std::cout<<symbols.list.size()<<std::endl;
-   }
+    static void chatterCallback(const navigator_shoot_vision::Symbols &symbols) {
+        syms = symbols;
+        // std::cout<<symbols.list.size()<<std::endl;
+    }
 
-  static  bool test(navigator_shoot_vision::GetShape::Request &req, navigator_shoot_vision::GetShape::Response &res) {
-//        for(int i = 0; i < syms.size(); i++) {
-        	for(int j =0; j < syms.list.size(); j++) {
-        		if(req.Shape == syms.list[j].Shape && req.Color == syms.list[j].Color) {
-        			res.CenterX = syms.list[j].CenterX;
-        			res.CenterY = syms.list[j].CenterY;
-        			return true;
-        		}
-   			 }
-   			    			 return false;
-   			 }
+    static bool test(navigator_shoot_vision::GetShape::Request &req, navigator_shoot_vision::GetShape::Response &res) {
+        //        for(int i = 0; i < syms.size(); i++) {
+        for (int j = 0; j < syms.list.size(); j++) {
+            if (req.Shape == syms.list[j].Shape && req.Color == syms.list[j].Color) {
+                res.CenterX = syms.list[j].CenterX;
+                res.CenterY = syms.list[j].CenterY;
+                return true;
+            }
+        }
+        return false;
+    }
 
-//   			 }
+    //   			 }
 };
-
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "smart_shape_finder_server");
-	ImageSearcher imageSearcher;
+    ImageSearcher imageSearcher;
     ros::spin();
     return 0;
 }
