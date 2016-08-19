54322d369b37b40580bdbecc10f6934fd33c79eb
RustyBamboo <dvolya@gmail.com>
2016-08-19 13:31:47 -0400

    Frame-aware shape detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,102 @@
 #include "std_msgs/String.h"
 
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
 #include "FrameProc.h"
 #include "ShapeFind.h"
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
 
+#include "DebugWindow.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/opencv.hpp"
-#include "DebugWindow.h"
 
+#include "navigator_shoot_vision/GetShapeController.h"
 #include "navigator_shoot_vision/Symbols.h"
 
 using namespace cv;
 
-class ShooterVision
-{
-	private:
-		//ros frame thing
-		navigator_shoot_vision::Symbols symbols;
-		FrameProc fp;
-		ShapeFind blueFinder;
-		ShapeFind redFinder;
-		ShapeFind greenFinder;
-		
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  ros::Publisher chatter_pub;
-	public:
-		
-	ShooterVision() : 
-    it_(nh_),
-		fp(),
-		blueFinder(navigator_shoot_vision::Symbol::BLUE),
-		redFinder(navigator_shoot_vision::Symbol::RED),
-		greenFinder(navigator_shoot_vision::Symbol::GREEN)
-	{
-		DebugWindow::init();
-		chatter_pub = nh_.advertise<navigator_shoot_vision::Symbols>("found_shapes", 1000);
-    image_sub_ = it_.subscribe("/camera/image_color", 1, &ShooterVision::run, this);
-		symbols = navigator_shoot_vision::Symbols();
-	}
+class ShooterVision {
+  private:
+    // ros frame thing
+    navigator_shoot_vision::Symbols symbols;
+    FrameProc fp;
+    ShapeFind blueFinder;
+    ShapeFind redFinder;
+    ShapeFind greenFinder;
 
-	void run(const sensor_msgs::ImageConstPtr& msg)
-	{
-		//Grab ros frame
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+    ros::Publisher chatter_pub;
+
+    ros::ServiceServer serviceCommand;
+    bool active;
+
+  public:
+    ShooterVision()
+        : it_(nh_), fp(), blueFinder(navigator_shoot_vision::Symbol::BLUE), redFinder(navigator_shoot_vision::Symbol::RED),
+          greenFinder(navigator_shoot_vision::Symbol::GREEN) {
+        active = false;
+
+        serviceCommand = nh_.advertiseService("GetShapeController", &ShooterVision::getShapeController, this);
+
+        	std::cout<<"hey!"<<std::endl;
+            DebugWindow::init();
+            chatter_pub = nh_.advertise<navigator_shoot_vision::Symbols>("found_shapes", 1000);
+            image_sub_ = it_.subscribe("/camera/image_color", 1, &ShooterVision::run, this);
+
+            symbols = navigator_shoot_vision::Symbols();
+        
     }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+
+    bool getShapeController(navigator_shoot_vision::GetShapeController::Request &req, navigator_shoot_vision::GetShapeController::Response &res) {
+        active = req.Active;
+        res.Meh = active;
+        std::cout<<"Setting active to "<<active<<std::endl;
+        return true;
     }
-//    std::cout << "Loop. Rows=" << cv_ptr->image.rows << std::endl;
-		//Convert Ros frame to opencv
-     cv::waitKey(3);
-		//Process frame
-		fp.Prepare(cv_ptr->image);
-		symbols.list.clear();
-		
-		//Find shapes in each color
-		blueFinder.GetSymbols(fp.GetBlue(),&symbols);
-		redFinder.GetSymbols(fp.GetRed(),&symbols);
-		greenFinder.GetSymbols(fp.GetGreen(),&symbols);
-		//Publish to ros
-		DebugWindow::UpdateResults(symbols);
-		chatter_pub.publish(symbols);
-	}
+    void run(const sensor_msgs::ImageConstPtr &msg) {
+        // Grab ros frame
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        } catch (cv_bridge::Exception &e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        //    std::cout << "Loop. Rows=" << cv_ptr->image.rows << std::endl;
+        // Convert Ros frame to opencv
+        cv::waitKey(3);
+        if(active) {
+        // Process frame
+        fp.Prepare(cv_ptr->image);
+        symbols.list.clear();
+
+        // Find shapes in each color
+        blueFinder.GetSymbols(fp.GetBlue(), &symbols);
+        redFinder.GetSymbols(fp.GetRed(), &symbols);
+        greenFinder.GetSymbols(fp.GetGreen(), &symbols);
+        // Publish to ros
+        for(int i =0 ; i < symbols.list.size(); i++) {
+        	std::cout<<symbols.list[i]<<std::endl;
+        }
+        DebugWindow::UpdateResults(symbols);
+        chatter_pub.publish(symbols);
+        }
+    }
 };
 
-int main(int argc,char ** argv)
-{
-  ros::init(argc, argv, "image_converter");
-	ShooterVision sv = ShooterVision();
-	while (waitKey(50) == -1 && ros::ok())
-	{
-    ros::spin();
-		//sv.run();
-	}
-	std::cout << "Key detected, exiting" << std::endl;
-	
-	//Some test code
-//	for(int i =0; i < DebugWindow::allFoundSymbols.size(); i++) {
-//		for(int a = 0; a < DebugWindow::allFoundSymbols[i].list.size(); a++) {
-//			for(int j =0; j < DebugWindow::allFoundSymbols.size(); j++) {
-//				for(int b = 0; b < DebugWindow::allFoundSymbols[j].list.size(); b++) {
-//					if(DebugWindow::allFoundSymbols[i].list[a].Color == DebugWindow::allFoundSymbols[j].list[b].Color) {
-//						if(DebugWindow::allFoundSymbols[i].list[a].Shape == DebugWindow::allFoundSymbols[j].list[b].Shape) {
-//							int vx = DebugWindow::allFoundSymbols[i].list[a].CenterX - DebugWindow::allFoundSymbols[j].list[b].CenterX;
-//							int vy = DebugWindow::allFoundSymbols[i].list[a].CenterY - DebugWindow::allFoundSymbols[j].list[b].CenterY;
-//							if(vx*vx + vy*vy < 10000) std::cout<< DebugWindow::allFoundSymbols[i].list[a].Shape<<std::endl;
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-
-//int sumx = 0;
-//int sumy = 0;	
-
-//	for(int i =0; i < DebugWindow::allFoundSymbols.size(); i++) {
-//		int tsumx = 0, tsumy = 0;
-//		for(int j = 0; j < DebugWindow::allFoundSymbols[i].list.size(); j++) {
-//				tsumx += DebugWindow::allFoundSymbols[i].list[j].CenterX;
-//				tsumy += DebugWindow::allFoundSymbols[i].list[j].CenterY;
-//		}
-//		tsumx /=DebugWindow::allFoundSymbols[i].list.size();
-//		tsumy /= DebugWindow::allFoundSymbols[i].list.size();
-//		
-//		sumx += tsumx;
-//		sumy += tsumy;
-//	}
-//	sumx /= DebugWindow::allFoundSymbols.size();
-//	sumy /= DebugWindow::allFoundSymbols.size();
-//	std::cout<<sumx<<" "<<sumy<<std::endl;
-
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "image_converter");
+    ShooterVision sv = ShooterVision();
+    while (waitKey(50) == -1 && ros::ok()) {
+        ros::spin();
+        // sv.run();
+    }
+    std::cout << "Key detected, exiting" << std::endl;
+    return 0;
 }
--- src/smart.cpp
+++ src/smart.cpp
@@ -18,37 +18,96 @@
 #include "navigator_shoot_vision/GetShape.h"
 #include "navigator_shoot_vision/Symbols.h"
 
-
-
 class ImageSearcher {
   private:
     ros::NodeHandle n;
     ros::Subscriber sub;
     ros::ServiceServer service;
-	navigator_shoot_vision::Symbols syms;
+    navigator_shoot_vision::Symbols syms; //latest frame
+    std::vector<navigator_shoot_vision::Symbols> frameSymbolHolder; //Previous frames
+    std::vector<navigator_shoot_vision::Symbol> possibleSymbols; 
+    int possibleShapes[3] = {112, 116, 99};
+    int possibleColors[3] = {114, 98, 103};
+    int counter[3 * 3];
+    int frames;
 
+	int lastCallFrame;
   public:
-  	std::vector<navigator_shoot_vision::Symbols> frameSymbolHolder;
     ImageSearcher() {
-        syms = navigator_shoot_vision::Symbols();
-        sub = n.subscribe("found_shapes", 1000, &ImageSearcher::chatterCallback, this);
-        service = n.advertiseService("GetShape", &ImageSearcher::test, this);
-    }
-   void chatterCallback(const navigator_shoot_vision::Symbols &symbols) {
-        syms = symbols;
-        frameSymbolHolder.push_back(symbols);
-        // std::cout<<symbols.list.size()<<std::endl;
-    }
-
-   bool test(navigator_shoot_vision::GetShape::Request &req, navigator_shoot_vision::GetShape::Response &res) {
-        //        for(int i = 0; i < syms.size(); i++) {
-        for (int j = 0; j < syms.list.size(); j++) {
-            if (req.Shape == syms.list[j].Shape && req.Color == syms.list[j].Color) {
-                res.CenterX = syms.list[j].CenterX;
-                res.CenterY = syms.list[j].CenterY;
-                return true;
+        frames = 0;
+        lastCallFrame = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                navigator_shoot_vision::Symbol holdSym;
+                holdSym.Shape = possibleShapes[i];
+                holdSym.Color = possibleColors[j];
+                holdSym.CenterX = 0;
+                holdSym.CenterY = 0;
+                possibleSymbols.push_back(holdSym);
+                counter[i * j] = 0;
             }
         }
+        std::cout << "Set" << std::endl;
+        syms = navigator_shoot_vision::Symbols();
+        sub = n.subscribe("found_shapes", 1000, &ImageSearcher::chatterCallback, this);
+        service = n.advertiseService("GetShape", &ImageSearcher::getShape, this);
+    }
+
+    float mean(int val, int size) { return val / size; }
+    void shapeChecker(const navigator_shoot_vision::Symbols &symbols) {
+
+        for (int i = 0; i < symbols.list.size(); i++) {
+            for (int k = 0; k < possibleSymbols.size(); k++) {
+
+                if (symbols.list[i].Shape == possibleSymbols[k].Shape && symbols.list[i].Color == possibleSymbols[k].Color) {
+                    if (frames < 10) {
+
+                        possibleSymbols[k].CenterX += symbols.list[i].CenterX;
+                        possibleSymbols[k].CenterY += symbols.list[i].CenterY;
+                        counter[k]++;
+
+                    } else if (std::abs(symbols.list[i].CenterX - mean(possibleSymbols[k].CenterX, counter[k])) < 100 &&
+                               std::abs(symbols.list[i].CenterY - mean(possibleSymbols[k].CenterY, counter[k])) < 100) {
+                        possibleSymbols[k].CenterX += symbols.list[i].CenterX;
+                        possibleSymbols[k].CenterY += symbols.list[i].CenterY;
+                        counter[k]++;
+                    }
+                }
+            }
+        }
+    }
+
+    void chatterCallback(const navigator_shoot_vision::Symbols &symbols) {
+        frames++;
+        syms = symbols;
+        frameSymbolHolder.push_back(symbols);
+        shapeChecker(symbols);
+    }
+
+    bool getShape(navigator_shoot_vision::GetShape::Request &req, navigator_shoot_vision::GetShape::Response &res) {
+    	if(frames == 0) {
+
+    	}
+        if (frames < 10) {
+            std::cout << "Too small of sample frames" << possibleSymbols.size() << std::endl;
+            return false;
+        }
+        for (int j = 0; j < syms.list.size(); j++) {
+            for (int i = 0; i < possibleSymbols.size(); i++) {
+
+                if (req.Shape == possibleSymbols[i].Shape && req.Color == possibleSymbols[i].Color && syms.list[j].Shape == req.Shape &&
+                    syms.list[j].Color == req.Color && counter[i] > 10) {
+                    if (std::abs(syms.list[j].CenterX - mean(possibleSymbols[i].CenterX, counter[i])) < 100 &&
+                        std::abs(syms.list[j].CenterY - mean(possibleSymbols[i].CenterY, counter[i])) < 100) {
+                        res.CenterX = syms.list[j].CenterX;
+                        res.CenterY = syms.list[j].CenterY;
+                        lastCallFrame = frames;
+                        return true;
+                    }
+                }
+            }
+        }
+        std::cout << "not found" << std::endl;
         return false;
     }
 
